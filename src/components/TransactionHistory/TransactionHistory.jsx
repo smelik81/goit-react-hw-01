5a6bdc35a5a9b3fe2fd0993b7e578fc277342768
smelik81 <artem.smelyanskiy81@gmail.com>
2024-06-12 22:24:43 +0300

    finished task-3

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,5 +1,36 @@
-import css from "./TransactionHistory.module.css";
+import css from "./Transaction.module.css";
+import clsx from "clsx";
 
-export default function TransactionHistory() {
-  return <div className={css.container}></div>;
+export default function TransactionHistory({ items }) {
+  return (
+    <>
+      <table className={css.container}>
+        <thead className={css.thead}>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+
+        <tbody className={css.tbody}>
+          {items.map(({ id, type, amount, currency }) => {
+            const classTable = clsx(css.colorTable, {
+              [css.invoice]: type === "invoice",
+              [css.payment]: type === "payment",
+              [css.withdrawal]: type === "withdrawal",
+              [css.deposit]: type === "deposit",
+            });
+            return (
+              <tr key={id}>
+                <td className={classTable}>{type}</td>
+                <td className={classTable}>{amount}</td>
+                <td className={classTable}>{currency}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
