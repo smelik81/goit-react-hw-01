739045bb1515c0fb932b1e2352fce6e4038c5a0e
smelik81 <artem.smelyanskiy81@gmail.com>
2024-06-16 22:26:44 +0300

    update way for filr

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
+import FriendListItem from "../FriendListItem/FriendListItem";
 import css from "./FriendList.module.css";
-import FriendListItem from "./FriendListItem";
 
 export default function FriendList({ friends }) {
   return (
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,4 +1,4 @@
-import css from "./FriendList.module.css";
+import css from "../FriendList/FriendList.module.css";
 
 export default function FriendListItem({ name, avatar, isOnline }) {
   return (
